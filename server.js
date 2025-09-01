7faeb78fdf0e96ba7121950a6850c642e3c56479
unknown <mouzzamdanishh@gmail.com>
2025-09-01 05:41:51 +0500

    New

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ connectDB()
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: [
+      process.env.CLIENT_URL || "http://localhost:3000",
+      "http://localhost:3000",
+      "https://tnttierion.com"
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
